2d01a9d8ea1be4ddb3473683ca976c6546ec6941
priyapatilp2007-ui <priyapatilp2007@gmail.com>
2026-09-25 16:27:25 +0530

    Update vowels.py

--- vowels.py
+++ vowels.py
@@ -1,6 +1,7 @@
 String=input("Enter the word)
 count = 0
 for ch in string:
-  if ch.lower in "aeiou"
+  if ch.lower in "aeiou":
       count +=1
   print("Number of vowels:",count);
+  print("The vowels are:")
